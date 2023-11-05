a4fec13cdd5fbb0b91cf22678ba0182fa53500ac
ADMIN <ADMIN@DESKTOP-V3LBF7M>
2023-11-05 21:41:11 +0530

    Push to project class1

---
+++ src/com/datatype/DataTypes.java
@@ -0,0 +1,32 @@
+package com.datatype;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		String name = "Deva";
+
+		byte age = 34;
+
+		short id = 1234;
+
+		int phone = 234567;
+
+		long accountno = 3350474287l;
+
+		float mark = 89.234f;
+
+		double salary = 34567.872d;
+
+		char male = 'M';
+
+		boolean status = true;
+
+		System.out.println("Teacher Nmae is: " + name + "\n" + "Teacher Age is: " + age + "\n" + "Teacher Id is: " + id
+				+ "\n" + "Teacher Phone No is: " + phone + "\n" + "Teacher Account No is: " + accountno + "\n"
+				+ "Teacher Percentage is: " + mark + "\n" + "Teacher Salary is: " + salary + "\n" + "Teacher Gender is:"
+				+ male + "\n" + "Teacher Status is: " + status);
+
+	}
+
+}
